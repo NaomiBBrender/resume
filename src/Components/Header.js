c7012d0a36ba779e36cfdc54f612cdb45b8cff0f
Naomi Brender <NaomiBBrender@gmail.com>
2023-04-10 14:44:39 -0400

    hiding contact form

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,50 +1,50 @@
-import { React } from "react";
-import Container from "react-bootstrap/Container";
+import { React } from 'react';
+import Container from 'react-bootstrap/Container';
 // import About from "./About";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import {
   Row,
   Col,
   Navbar,
   // NavDropdown,
-} from "react-bootstrap";
+} from 'react-bootstrap';
 
 // import About from "./About";
 
 function Header() {
   return (
-    <div className="App-header">
+    <div className='App-header'>
       <Router>
         <Navbar>
           <Container>
             <Row>
-              <Col id="header-name">
-                <a className="linkText" href="/">
+              <Col id='header-name'>
+                <a className='linkText' href='/'>
                   NAOMI
                 </a>
               </Col>
             </Row>
-            <Row id="header-links">
+            <Row id='header-links'>
               <Col>
-                <a className="linkText" href="./about">
+                <a className='linkText' href='./about'>
                   About
                 </a>
               </Col>
               <Col>
-                <a className="linkText" href="./work">
+                <a className='linkText' href='./work'>
                   Work
                 </a>
               </Col>
               <Col>
-                <a className="linkText" href="./play">
+                <a className='linkText' href='./play'>
                   Play
                 </a>
               </Col>
-              <Col>
+              {/* <Col>
                 <a className="linkText" href="./contact">
                   Contact
                 </a>
-              </Col>
+              </Col> */}
             </Row>
           </Container>
         </Navbar>
