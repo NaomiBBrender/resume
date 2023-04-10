9c4573012da47c3f9adebfea8a65347d64466500
Naomi Brender <NaomiBBrender@gmail.com>
2023-04-10 16:51:21 -0400

    lisa quest gif

--- src/Components/work/LisaQuest.js
+++ src/Components/work/LisaQuest.js
@@ -6,8 +6,30 @@ const LisaQuest = () => {
     <div className='lisaQuestPage'>
       <h1>Check out Lisa Quest!</h1>
       <a id='about-link' href='https://lisaquest.onrender.com/'>
-        <strong id='about-link'>CLICK HERE TO PLAY LISA QUEST</strong>
+        <strong id='about-link'>
+          {' '}
+          {'--> CLICK HERE TO PLAY LISA QUEST <--'}
+        </strong>
       </a>
+      <div>
+        <iframe
+          src='https://giphy.com/embed/uk7idkHsu5se8R4zuS'
+          width='480'
+          height='384'
+          // frameBorder='0'
+          class='giphy-embed'
+          // allowFullScreen
+        ></iframe>
+      </div>
+      <p>
+        <a href='https://giphy.com/gifs/game-hearts-lisa-uk7idkHsu5se8R4zuS'>
+          via GIPHY
+        </a>
+      </p>
+      <div>
+        A classic platformer game in space. Technologies Used: Javascript,
+        HTML/CSS, Redux, Phaser3, Tiled, OnRender, Bit.io.
+      </div>
     </div>
   );
 };
