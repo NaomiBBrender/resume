bfdb126d14d293ce66a3cfeb472b589dcf947c42
Naomi Brender <NaomiBBrender@gmail.com>
2023-04-10 16:13:36 -0400

    working on chef Hopper page

--- src/Components/work/ChefHopper.js
+++ src/Components/work/ChefHopper.js
@@ -1,34 +1,38 @@
-import { React } from "react";
-import { Card, Row, Col, Container } from "react-bootstrap";
+import { React } from 'react';
+import { Card, Row, Col, Container } from 'react-bootstrap';
 
 const ChefHopper = () => {
   return (
-    <div className="chefHopperPage">
-      <Container i>
+    <div className='chefHopperPage'>
+      <Container>
         <Row>
           {/* <Col> */}
-            <Card id="code-card">
-              <Card.Img id="code-image" src="/images/chefhopperbakery.png" rounded />
-            </Card>
+          <Card id='code-card'>
+            <Card.Img
+              className='code-image'
+              // width='100'
+              src='/images/chefhopperbakery.png'
+              rounded
+            />
+          </Card>
           {/* </Col> */}
           {/* <Col> */}
-            <Card id="code-card">
-              <Card.Body>
-                <Card.Text>
-                  My team and I created and{" "}
-                  <a href="https://chef-hopper-bakery.onrender.com/login">
-                    <strong id="about-link">deployed</strong>
-                  </a>{" "}
-                  a mock ecom website using PostreSQL, Sequlize, React, and
-                  Redux.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+          <Card id='code-card'>
+            <Card.Body>
+              <Card.Text>
+                My team and I created and{' '}
+                <a href='https://chef-hopper-bakery.onrender.com/login'>
+                  <strong id='about-link'>deployed</strong>
+                </a>{' '}
+                a mock ecom website using PostreSQL, Sequlize, React, and Redux.
+              </Card.Text>
+            </Card.Body>
+          </Card>
           {/* </Col> */}
         </Row>
         <Row>
           <Col>
-            <Card id="code-card">
+            <Card id='code-card'>
               <Card.Text>
                 talking about our process, talking about our process, talking
                 about our process, talking about our process, talking about our
@@ -37,19 +41,19 @@ const ChefHopper = () => {
             </Card>
           </Col>
           <Col>
-            <Card id="code-card">
-              <Card.Img src="/images/brainstormingGraceShopper.png" rounded />
+            <Card id='code-card'>
+              <Card.Img src='/images/brainstormingGraceShopper.png' rounded />
             </Card>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Card id="code-card">
-              <Card.Img src="/images/brainstormingGraceShopperRoutes.png" />
+            <Card id='code-card'>
+              <Card.Img src='/images/brainstormingGraceShopperRoutes.png' />
             </Card>
           </Col>
           <Col>
-            <Card id="code-card">
+            <Card id='code-card'>
               <Card.Text>
                 talking about our process, talking about our process, talking
                 about our process, talking about our process, talking about our
@@ -60,7 +64,7 @@ const ChefHopper = () => {
         </Row>
         <Row>
           <Col>
-            <Card id="code-card">
+            <Card id='code-card'>
               <Card.Text>
                 talking about our process, talking about our process, talking
                 about our process, talking about our process, talking about our
@@ -69,8 +73,8 @@ const ChefHopper = () => {
             </Card>
           </Col>
           <Col>
-            <Card id="code-card">
-              <Card.Img src="/images/graceShopperCodePhoto.png" rounded />
+            <Card id='code-card'>
+              <Card.Img src='/images/graceShopperCodePhoto.png' rounded />
             </Card>
           </Col>
         </Row>
